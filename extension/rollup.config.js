96ec6bd18f366966d392292d5c026c76fe2ea761
Philipp Hoenisch <philipp@hoenisch.at>
2021-02-10 11:44:15 +1100

    combine rollup config

--- extension/rollup.config.js
+++ extension/rollup.config.js
@@ -1,30 +1,54 @@
 import rust from "@wasm-tool/rollup-plugin-rust";
 import copy from 'rollup-plugin-copy'
 
-export default {
-  input: {
-    background: "src/background/Cargo.toml",
-    // content: "src/content/Cargo.toml"
+  export default [{
+    input: {
+      background: "src/background/Cargo.toml",
+    },
+    output: {
+      dir: "dist",
+      format: "esm",
+      sourcemap: true,
+      // TODO source map URL is missing the js/
+      entryFileNames: "js/[name].js",
+    },
+    plugins: [
+      rust({
+        outDir: "js",
+        importHook: function (path) {
+          return "browser.runtime.getURL(" + JSON.stringify(path) + ")";
+        },
+      }),
+      copy({
+        targets: [
+          {src: 'static/*', dest: 'dist'},
+        ]
+      })
+    ],
   },
-  output: {
-    dir: "dist",
-    format: "esm",
-    sourcemap: true,
-    // TODO source map URL is missing the js/
-    entryFileNames: "js/[name].js",
-  },
-  plugins: [
-    rust({
-      outDir: "js",
-      importHook: function (path) {
-        return "browser.runtime.getURL(" + JSON.stringify(path) + ")";
+    {
+      input: {
+        content: "src/content/Cargo.toml"
       },
-    }),
-    copy({
-      targets: [
-        { src: 'static/*', dest: 'dist' },
-        { src: 'src/content/dist/*', dest: 'dist' },
-      ]
-    })
-  ],
-};
+      output: {
+        dir: "dist",
+        format: "iife",
+        sourcemap: true,
+        // TODO source map URL is missing the js/
+        entryFileNames: "js/[name].js",
+      },
+      plugins: [
+        rust({
+          outDir: "js",
+          importHook: function (path) {
+            return "browser.runtime.getURL(" + JSON.stringify(path) + ")";
+          },
+        }),
+        copy({
+          targets: [
+            {src: 'src/content/dist/*', dest: 'dist'},
+          ]
+        })
+      ],
+    }
+];
