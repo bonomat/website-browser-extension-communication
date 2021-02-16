605f5152f375214c0b31c3df43b1d562e2f4c76d
Philipp Hoenisch <philipp@hoenisch.at>
2021-02-16 15:27:03 +1100

    [WIP] Response from BS to CS working, crashing on IPS again

--- extension/src/in_page/src/lib.rs
+++ extension/src/in_page/src/lib.rs
@@ -1,8 +1,11 @@
-use js_sys::Object;
+use futures::channel::oneshot;
+use futures::TryFutureExt;
 use js_sys::{global, Promise};
+use js_sys::{Function, Object};
 use std::future::Future;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::{spawn_local, JsFuture};
+use wasm_bindgen_futures::{future_to_promise, spawn_local, JsFuture};
+use web_sys::MessageEvent;
 
 #[wasm_bindgen(start)]
 pub fn main() {
@@ -11,7 +14,7 @@ pub fn main() {
 
     let global: Object = global();
 
-    let boxed = Box::new(call_backend) as Box<dyn Fn(String)>;
+    let boxed = Box::new(call_backend) as Box<dyn Fn(String) -> Promise>;
     let add_closure = Closure::wrap(boxed);
     let add_closure = add_closure.into_js_value();
 
@@ -19,21 +22,38 @@ pub fn main() {
 }
 
 #[wasm_bindgen]
-pub fn call_backend(txt: String) {
+pub fn call_backend(txt: String) -> Promise {
     let js_value = JsValue::from(txt);
-    let future = window.post_message(js_value);
 
-    // let future = promise.clone();
-    // log::info!("IPS: Received response from CS: {:?}", response);
-    // TODO read from the future
-    spawn(async move {
-        let resp = JsFuture::from(future).await?;
-        log::info!(
-            "IPS: Received response from CS: {:?}",
-            resp.as_string().is_some()
-        );
-        Ok(())
-    });
+    let (sender, receiver) = oneshot::channel::<JsValue>();
+    // create listener
+    let func = move |msg: MessageEvent| {
+        if msg.origin() == localhost
+            return;
+        let js_value: JsValue = msg.data();
+        let string = js_value.as_string().unwrap();
+        log::info!("IPS: Received from {:?}: {:?}", msg.origin(), string);
+
+        // callback(string);
+        sender.send(JsValue::from_str(&string));
+        return Promise::resolve(&JsValue::from(""));
+    };
+
+    let closure = Closure::once(func);
+
+    // TODO remove event listener again
+    window.add_event_listener("message".to_string(), &closure);
+
+    window.post_message(js_value);
+    closure.forget();
+
+    return future_to_promise(
+        receiver
+            .inspect_ok(|resp| {
+                log::info!("IPS: Received response from CS: {:?}", resp);
+            })
+            .map_err(|er| JsValue::from(er.to_string())),
+    );
 }
 
 #[wasm_bindgen]
@@ -45,6 +65,14 @@ extern "C" {
 
     #[wasm_bindgen(method, js_name = postMessage)]
     pub fn post_message(this: &Window, value: JsValue) -> Promise;
+
+    #[wasm_bindgen(method, js_name = addEventListener)]
+    pub fn add_event_listener(
+        this: &Window,
+        event: String,
+        // TODO: do we have to use MessageEvent here?
+        closure: &Closure<dyn FnMut(MessageEvent) -> Promise>,
+    ) -> JsValue;
 }
 
 pub fn unwrap_future<F>(future: F) -> impl Future<Output = ()>
