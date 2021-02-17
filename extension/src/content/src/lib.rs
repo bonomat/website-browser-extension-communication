2ca3847ddc96ffbf0935275bc7c75090c6a02fe4
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2021-02-18 10:18:47 +1100

    Use web_sys::Window in content-script

--- extension/src/content/src/lib.rs
+++ extension/src/content/src/lib.rs
@@ -2,7 +2,7 @@ use js_sys::Promise;
 use serde::{Deserialize, Serialize};
 use std::future::Future;
 use wasm_bindgen::{prelude::*, JsCast};
-use wasm_bindgen_extention::{browser, window};
+use wasm_bindgen_extention::browser;
 use wasm_bindgen_futures::{spawn_local, JsFuture};
 use web_sys::MessageEvent;
 
@@ -32,7 +32,8 @@ pub async fn main() -> Result<(), JsValue> {
         .unwrap();
 
     // create listener
-    let func = |msg: MessageEvent| {
+    let func = |msg: JsValue| {
+        let msg: MessageEvent = msg.into();
         let js_value: JsValue = msg.data();
         // TODO: Actually only accept messages from IPS
         if let Some(string) = js_value.as_string() {
@@ -45,25 +46,29 @@ pub async fn main() -> Result<(), JsValue> {
             // sending message to Background script
             let js_value = JsValue::from_serde(&msg).unwrap();
             let resp: Promise = browser.runtime().send_message(js_value);
-            spawn(async move {
+            spawn(async {
+                let rs_window = web_sys::window().expect("no global `window` exists");
                 let resp = JsFuture::from(resp).await?;
                 log::info!("CS: Received response from BS: {:?}", resp);
 
-                window.post_message(
-                    JsValue::from_serde(&Message {
+                rs_window.post_message(
+                    &JsValue::from_serde(&Message {
                         data: resp.as_string().unwrap(),
                         target: "in-page".to_string(),
                         source: Some("content".to_string()),
                     })
                     .unwrap(),
-                );
+                    "*",
+                )?;
                 Ok(())
             });
         }
     };
 
     let cb = Closure::wrap(Box::new(func) as Box<dyn Fn(_)>);
-    window.add_event_listener("message", cb.as_ref().unchecked_ref());
+    rs_window
+        .add_event_listener_with_callback("message", cb.as_ref().unchecked_ref())
+        .unwrap();
 
     cb.forget();
 
@@ -77,14 +82,12 @@ struct Message {
     source: Option<String>,
 }
 
-pub fn unwrap_future<F>(future: F) -> impl Future<Output = ()>
+pub async fn unwrap_future<F>(future: F)
 where
     F: Future<Output = Result<(), JsValue>>,
 {
-    async {
-        if let Err(e) = future.await {
-            log::error!("{:?}", &e);
-        }
+    if let Err(e) = future.await {
+        log::error!("{:?}", &e);
     }
 }
 
