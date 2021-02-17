a3cb8a791d554f98570bb536eda67324283b8f27
Philipp Hoenisch <philipp@hoenisch.at>
2021-02-18 10:03:32 +1100

    Use wasm_bindgen_extention in content

--- extension/src/content/src/lib.rs
+++ extension/src/content/src/lib.rs
@@ -2,47 +2,10 @@ use js_sys::Promise;
 use serde::{Deserialize, Serialize};
 use std::future::Future;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_extention::{browser, window};
 use wasm_bindgen_futures::{spawn_local, JsFuture};
 use web_sys::MessageEvent;
-
-#[wasm_bindgen]
-extern "C" {
-    pub type Browser;
-    pub static browser: Browser;
-
-    #[wasm_bindgen(method, getter)]
-    pub fn runtime(this: &Browser) -> Runtime;
-}
-
-#[wasm_bindgen]
-extern "C" {
-    pub type Runtime;
-
-    #[wasm_bindgen(method, js_name = getURL)]
-    pub fn get_url(this: &Runtime, path: String) -> String;
-
-    #[wasm_bindgen(method, js_name = sendMessage)]
-    pub fn send_message(this: &Runtime, value: JsValue) -> Promise;
-}
-
-#[wasm_bindgen]
-extern "C" {
-    #[derive(Debug)]
-    pub type Window;
-
-    pub static window: Window;
-
-    #[wasm_bindgen(method, js_name = postMessage)]
-    pub fn post_message(this: &Window, value: JsValue);
-
-    #[wasm_bindgen(method, js_name = addEventListener)]
-    pub fn add_event_listener(
-        this: &Window,
-        event: String,
-        // TODO: do we have to use MessageEvent here?
-        closure: &Closure<dyn Fn(MessageEvent)>,
-    ) -> JsValue;
-}
 
 #[wasm_bindgen(start)]
 pub async fn main() -> Result<(), JsValue> {
@@ -101,7 +64,7 @@ pub async fn main() -> Result<(), JsValue> {
     };
 
     let cb = Closure::wrap(Box::new(func) as Box<dyn Fn(_)>);
-    window.add_event_listener("message".to_string(), &cb);
+    window.add_event_listener("message", cb.as_ref().unchecked_ref());
 
     cb.forget();
 
